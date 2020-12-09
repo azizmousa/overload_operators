86a9e17de7b4be4f605291ebbf275c87c112187a
azizmousa <azizmousa1010@gmail.com>
2020-12-10 01:36:15 +0200

    add the main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
-
+#include "overload_operators/String.h"
 
 int main(int argc, char *argv[]){
+    String string = "hello";
+    std::cout << string.get_string() << std::endl;
+    String s2{string}; 
+    std::cout << s2.get_string() << std::endl;
+    s2 = "welcome to my string:)";
+    std::cout << s2.get_string() << std::endl;
+    s2 = string;
+    std::cout << s2.get_string() << std::endl;
+    // string.display();
+    std::cout << string.get_string() << std::endl;
     return 0;
 }
